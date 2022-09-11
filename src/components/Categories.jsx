2f7daaed7e11c90e0ebe3fb6eec4401235d55e1d
Artiom Manokhin <amanokhin22@ukr.net>
2022-09-11 15:27:30 +0300

    Initialize project using Create React App

---
+++ src/components/Categories.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+
+const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const categories = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые'
+    ];
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index);
+    };
+
+    return (
+        <div className="categories">
+            <ul>
+                {
+                    categories.map((value, index) => (
+                        <li onClick={() => onClickCategory(index)}
+                            className={activeIndex === index ? 'active' : ''}>
+                            {value}
+                        </li>
+                    ))
+                }
+
+            </ul>
+        </div>
+    );
+}
+
+export default Categories
